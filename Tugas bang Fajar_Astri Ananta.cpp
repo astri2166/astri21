b7c32a462e0e95fb6c467b6c045495ca60010dd6
astri2166 <astriananta176@gmail.com>
2025-01-11 16:35:24 +0700

    Add files via upload

---
+++ Tugas bang Fajar_Astri Ananta.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Menampilkan informasi
+    std::cout << "Astri Ananta Kartini" << std::endl;
+    std::cout << "NIM     : 2412501872" << std::endl;
+    std::cout << "Alamat  : Jln. Swadaya Rt 07/01" << std::endl;
+    std::cout << "No. TLP : 083894337491" << std::endl;
+
+    return 0;
+}
+
